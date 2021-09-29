f69e25fdb5cc78858db87ee28cde7460b9e3d147
Ivan Pfanenshtil <85278763+IPfa@users.noreply.github.com>
2021-09-29 22:50:05 +0200

    Add files via upload

---
+++ hamiltonbot.py
@@ -0,0 +1,58 @@
+import datetime as dt
+import os
+import time
+
+import telegram
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+CURRENT_DATE = dt.datetime.now().date()
+UPDATE_TIME = 60 * 5
+UPDATE_TIME_ERROR = 60 * 60 * 24
+HAMOLTON_TOOLS = {
+    'Reader Check Plate _1659_\n 21.05.2022': dt.date(2022, 5, 21),
+    'Scale WXS _C112364454_\n 09.04.2022': dt.date(2022, 4, 9),
+    'Carrier VK _V0106115_\n 18.05.2023': dt.date(2023, 5, 18),
+    'Carrier SK _V0105708_\n 10.03.2023': dt.date(2023, 3, 10),
+    'IR Sensor _20100710_\n 05.02.2022': dt.date(2022, 2, 5),
+    'HMD _83208494_\n 16.09.2022': dt.date(2022, 9, 16),
+    'Calibration Weight (20 g) _C114466011_\n 15.03.2023': dt.date(2023, 3, 15),
+    'WIKA Pressure Sensor _2280270_\n 25.03.2022': dt.date(2022, 3, 25),
+    'Channel Calibration Tool _3891_\n 13.04.2023': dt.date(2023, 4, 13),
+    '384 Adjustment Tool _1296_\n 29.03.2023': dt.date(2023, 3, 29),
+    '96 Adjustment Tool _1558_\n 25.11.2022': dt.date(2022, 11, 25),
+}
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+
+def report_generator(current_date):
+    report = []
+    for i in HAMOLTON_TOOLS:
+        date = (HAMOLTON_TOOLS[i] - current_date).days
+        if date <= dt.timedelta(days=90).days:
+            date = f'*{date}*'
+        report.append("*-* " + f'{i} - {date} days' + "\n")
+    return (
+        "*Hamilton tools calibration date expiry report:*\n"
+        + "\n" + "\n".join(report)
+    )
+
+
+def send_report(report):
+    bot.send_message(chat_id=CHAT_ID, text=report, parse_mode='markdown')
+
+
+def main():
+    while True:
+        try:
+            send_report(report_generator(CURRENT_DATE))
+            time.sleep(UPDATE_TIME)
+        except Exception as e:
+            send_report(f'Возникла ошибка - {e}')
+            time.sleep(UPDATE_TIME_ERROR)
+
+
+main()
